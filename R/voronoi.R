5bdca833678f92ee5b03760ecb1a70b5457c7c8d
stla <laurent_step@yahoo.fr>
2021-12-20 18:30:54 +0100

    voronoi

--- R/voronoi.R
+++ R/voronoi.R
@@ -13,21 +13,47 @@ edgesFromTileFacet <- function(tessellation, tilefacet){
   tile1 <- tiles[[tileindices[1L]]]
   c1 <- tile1[["simplex"]][["circumcenter"]]
   if(length(tileindices) == 1L){
-    return(IEdge$new(c1, tilefacet[["subsimplex"]][["normal"]]))
+    return(IEdge$new(c1, tilefacet[["normal"]]))
   }
   tile2 <- tiles[[tileindices[2L]]]
   c2 <- tile2[["simplex"]][["circumcenter"]]
   if(sameFamily(tile1, tile2) || isTRUE(all.equal(c1, c2))){
     return(NULL)
   }
-  Edge$new(center1, center2)
+  Edge$new(c1, c2)
 }
 
-voronoiCell <- function(facetsQuotienter, edgeTransformer, tessellation, vertexId){
-  tilefacets <- facetsQuotienter(vertexNeighborFacets(tessellation, vertexId))
-  edges <- Filter(Negate(is.null), lapply(tilefacets, function(tilefacet){
-    edgesFromTileFacet(tessellation, tilefacet)
-  }))
-  edgeTransformer(edges)
+voronoiCell <- function(facetsQuotienter, edgeTransformer){
+  function(tessellation, vertexId){
+    tilefacets <- facetsQuotienter(vertexNeighborFacets(tessellation, vertexId))
+    edges <- Filter(Negate(is.null), lapply(tilefacets, function(tilefacet){
+      edgesFromTileFacet(tessellation, tilefacet)
+    }))
+    edgeTransformer(edges)
+  }
+}
+
+#' @importFrom sets pair
+#' @noRd
+zip <- function(matrix, list){
+  lapply(seq_len(nrow(matrix)), function(i) pair(matrix[i, ], list[[i]]))
+}
+
+voronoi0 <- function(cellGetter, tessellation){
+  vertices = attr(tessellation, "points")
+  L <- lapply(1:nrow(vertices), function(i) cellGetter(tessellation, i))
+  zip(vertices, L)
+}
+
+#' Title
+#'
+#' @param tessellation
+#'
+#' @return
+#' @export
+#'
+#' @examples
+voronoi <- function(tessellation){
+  voronoi0(voronoiCell(identity, identity), tessellation)
 }
 
