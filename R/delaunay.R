38856194e7a79e18b17986fce5ec7b68f98c5f0e
stla <laurent_step@yahoo.fr>
2021-12-20 13:31:54 +0100

    plotDelaunay3D

--- R/delaunay.R
+++ R/delaunay.R
@@ -65,7 +65,7 @@ delaunay <- function(points, atinfinity = FALSE, degenerate = FALSE){
     tess[["tilefacets"]][[i]][["subsimplex"]][["vertices"]] <-
       hash(as.character(vertices), pointsAsList[vertices])
   }
-  attr(tess, "dimension") <- ncol(points)
+  attr(tess, "points") <- points
   tess
 }
 
@@ -100,4 +100,67 @@ getDelaunaySimplicies <- function(tessellation, hashes = FALSE){
     simplicies <- lapply(simplicies, function(simplex) t(values(simplex)))
   }
   simplicies
+}
+
+#' @title Plot 3D Delaunay tessellation
+#' @description Plot a 3D Delaunay tessellation.
+#'
+#' @param tesselation the output of \code{\link{delaunay}}
+#' @param color Boolean, whether to use colors
+#' @param alpha opacity, between 0 and 1
+#'
+#' @return No value, just renders a 3D plot.
+#' @export
+#' @importFrom randomcoloR randomColor
+#' @importFrom utils combn
+#' @importFrom rgl triangles3d
+#' @importFrom hash keys values
+#'
+#' @examples library(tessellation)
+#' pts <- rbind(
+#'   c(-5, -5,  16),
+#'   c(-5,  8,   3),
+#'   c(4,  -1,   3),
+#'   c(4,  -5,   7),
+#'   c(4,  -1, -10),
+#'   c(4,  -5, -10),
+#'   c(-5,  8, -10),
+#'   c(-5, -5, -10)
+#' )
+#'
+#' tess <- delaunay(pts)
+#' library(rgl)
+#' open3d(windowRect = c(50, 50, 562, 562))
+#' plotDelaunay3D(tess)
+plotDelaunay3D <- function(tesselation, color = TRUE, alpha = 0.3){
+  vertices <- attr(tesselation, "points")
+  if(ncol(vertices) != 3){
+    stop(
+      sprintf("Invalid dimension (%d instead of 3).", ncol(vertices)),
+      call. = TRUE
+    )
+  }
+  simplicies <- getDelaunaySimplicies(tesselation, hashes = TRUE)
+  edges <- unique(do.call(rbind, lapply(simplicies, function(simplex){
+    t(combn(as.integer(keys(simplex)), 2L))
+  })))
+  nsimplicies <- length(simplicies)
+  if(color){
+    colors <- randomColor(nsimplicies, hue = "random", luminosity = "light")
+  }else{
+    colors <- rep("white", nsimplicies)
+  }
+  triangles <- combn(4L, 3L)
+  for(i in 1L:nsimplicies){
+    simplex <- t(values(simplicies[[i]]))
+    for(j in 1L:4L){
+      triangles3d(simplex[triangles[, j], ], color = colors[i], alpha = alpha)
+    }
+  }
+  for(i in 1L:nrow(edges)){
+    edge <- edges[i, ]
+    p1 <- vertices[edge[1L], ]
+    p2 <- vertices[edge[2L], ]
+    lines3d(rbind(p1,p2), color = "black")
+  }
 }
