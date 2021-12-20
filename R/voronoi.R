8649beaaf1708993fd387f48d5a2a4040f6a3e8c
stla <laurent_step@yahoo.fr>
2021-12-20 16:21:35 +0100

    some functions

---
+++ R/voronoi.R
@@ -0,0 +1,24 @@
+sameFamily <- function(tile1, tile2){
+  family1 <- tile1[["family"]]
+  family2 <- tile2[["family"]]
+  if(is.na(family1) || is.na(family2)){
+    return(FALSE)
+  }
+  family1 == family2
+}
+
+edgesFromTileFacet <- function(tessellation, tilefacet){
+  tileindices <- tilefacet[["facetOf"]]
+  tiles <- tessellation[["tiles"]]
+  tile1 <- tiles[[tileindices[1L]]]
+  c1 <- tile1[["simplex"]][["center"]]
+  if(length(tileindices) == 1L){
+    return(IEdge$new(c1, tilefacet[["normal"]]))
+  }
+  tile2 <- tiles[[tileindices[2L]]]
+  c2 <- tile2[["simplex"]][["center"]]
+  if(sameFamily(tile1, tile2) || isTRUE(all.equal(c1, c2))){
+    return(NULL)
+  }
+  Edge$new(center1, center2)
+}
