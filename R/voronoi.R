ac55fdf75d2ac9c593deb39d6f20ea8ba7dfbdf6
stla <laurent_step@yahoo.fr>
2021-12-20 16:56:07 +0100

    some functions

--- R/voronoi.R
+++ R/voronoi.R
@@ -11,14 +11,23 @@ edgesFromTileFacet <- function(tessellation, tilefacet){
   tileindices <- tilefacet[["facetOf"]]
   tiles <- tessellation[["tiles"]]
   tile1 <- tiles[[tileindices[1L]]]
-  c1 <- tile1[["simplex"]][["center"]]
+  c1 <- tile1[["simplex"]][["circumcenter"]]
   if(length(tileindices) == 1L){
-    return(IEdge$new(c1, tilefacet[["normal"]]))
+    return(IEdge$new(c1, tilefacet[["subsimplex"]][["normal"]]))
   }
   tile2 <- tiles[[tileindices[2L]]]
-  c2 <- tile2[["simplex"]][["center"]]
+  c2 <- tile2[["simplex"]][["circumcenter"]]
   if(sameFamily(tile1, tile2) || isTRUE(all.equal(c1, c2))){
     return(NULL)
   }
   Edge$new(center1, center2)
 }
+
+voronoiCell <- function(facetsQuotienter, edgeTransformer, tessellation, vertexId){
+  tilefacets <- facetsQuotienter(vertexNeighborFacets(tessellation, vertexId))
+  edges <- Filter(Negate(is.null), lapply(tilefacets, function(tilefacet){
+    edgesFromTileFacet(tessellation, tilefacet)
+  }))
+  edgeTransformer(edges)
+}
+
