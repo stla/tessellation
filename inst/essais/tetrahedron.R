48e728875f91cd9d4fd2eb5a66b6f331c5e486aa
stla <laurent_step@yahoo.fr>
2021-12-27 18:47:21 +0100

    started vignette

---
+++ inst/essais/tetrahedron.R
@@ -0,0 +1,76 @@
+#library(tessellation)
+
+tetrahedron <-
+  rbind(
+    c(2*sqrt(2)/3, 0, -1/3),
+    c(-sqrt(2)/3, sqrt(2/3), -1/3),
+    c(-sqrt(2)/3, -sqrt(2/3), -1/3),
+    c(0, 0, 1)
+  )
+
+library(uniformly)
+set.seed(314)
+randomPoints <- runif_in_tetrahedron(
+  3, tetrahedron[1, ], tetrahedron[2, ], tetrahedron[3, ], tetrahedron[4, ]
+)
+
+library(rgl)
+plotTetrahedron <- function(tetrahedron, alpha){
+  faces <- combn(4L, 3L)
+  for(j in 1L:ncol(faces)){
+    triangles3d(tetrahedron[faces[, j], ], color = "orange", alpha = alpha)
+  }
+  edges <- combn(4L, 2L)
+  for(j in 1L:ncol(edges)){
+    shade3d(
+      cylinder3d(tetrahedron[edges[, j], ], sides = 60, radius = 0.03),
+      color = "yellow"
+    )
+  }
+  spheres3d(tetrahedron, radius = 0.05, color = "yellow")
+}
+
+open3d(windowRect = c(50, 50, 562, 562))
+bg3d("slategray")
+plotTetrahedron(tetrahedron, alpha = 0.3)
+spheres3d(randomPoints, radius = 0.04, color = "black")
+
+movie3d(
+  spin3d(axis = c(0, 0, 1), rpm = 12),
+  duration = 5, fps = 12,
+  movie = "Tetrahedron", dir = ".",
+  convert = "magick convert -dispose previous -loop 0 -delay 1x%d %s*.png %s.%s",
+  startTime = 1/60
+)
+
+library(tessellation)
+pts <- rbind(tetrahedron, randomPoints)
+del <- delaunay(pts)
+open3d(windowRect = c(50, 50, 562, 562))
+material3d(lwd = 2)
+plotDelaunay3D(del, color=TRUE, luminosity = "bright", alpha=0.5)
+
+movie3d(
+  spin3d(axis = c(0, 0, 1), rpm = 12),
+  duration = 5, fps = 12,
+  movie = "TetrahedronDelaunay", dir = ".",
+  convert = "magick convert -dispose previous -loop 0 -delay 1x%d %s*.png %s.%s",
+  startTime = 1/60
+)
+
+
+
+
+tiles <- getDelaunaySimplicies(del)
+for(i in seq_along(tiles)){
+  open3d(windowRect = c(50, 50, 306, 306), zoom = 0.8)
+  bg3d("slategray")
+  material3d(lwd = 2)
+  plotDelaunay3D(del, color=FALSE)
+  plotTetrahedron(tiles[[i]], alpha = 0.6)
+  rgl.snapshot(sprintf("TetrahedronTile%02d.png", i))
+  close3d()
+}
+
+
+
